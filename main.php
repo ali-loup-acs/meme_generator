102bee3369e86cc24c98d05ee1242eedfa4a9a28
Ali LOUP <ali.j@codeur.online>
2016-12-16 15:51:49 +0100

    Carrousel ajouter

--- main.php
+++ main.php
@@ -1,17 +1,47 @@
-<main>
-
-  <div class="container">
-      <div class="col-md-12">
-          <img src="images/1.jpg" alt="">
-          <img src="images/2.jpg" alt="">
-          <img src="images/3.jpg" alt="">
-          <img src="images/4.jpg" alt="">
-          <img src="images/5.jpg" alt="">
-      </div>
+<main class="container">
+  <div class="col-md-12">
+        <div class="carousel slide" id="myCarousel">
+          <div class="carousel-inner">
+                <div class="item active">
+                  <div class="col-lg-4 col-xs-4 col-md-4 col-sm-4">
+                    <a href="#"><img src="images/1.jpg" alt="">1</a></div>
+                  </div>
+                <div class="item">
+                  <div class="col-lg-4 col-xs-4 col-md-4 col-sm-4">
+                  <a href="#"><img src="images/1.jpg" alt="">2</a></div>
+                </div>
+                <div class="item">
+                  <div class="col-lg-4 col-xs-4 col-md-4 col-sm-4">
+                    <a href="#"><img src="images/1.jpg" alt="">3</a></div>
+                </div>
+                <div class="item">
+                  <div class="col-lg-4 col-xs-4 col-md-4 col-sm-4">
+                  <a href="#"><img src="images/1.jpg" alt="">4</a></div>
+                </div>
+                <div class="item">
+                  <div class="col-lg-4 col-xs-4 col-md-4 col-sm-4">
+                    <a href="#"><img src="images/1.jpg" alt="">5</a></div>
+                </div>
+                <div class="item">
+                  <div class="col-lg-4 col-xs-4 col-md-4 col-sm-4">
+                  <a href="#"><img src="images/1.jpg" alt="">6</a></div>
+                </div>
+                <div class="item">
+                  <div class="col-lg-4 col-xs-4 col-md-4 col-sm-4">
+                    <a href="#"><img src="images/1.jpg" alt="">7</a></div>
+                  </div>
+                <div class="item">
+                  <div class="col-lg-4 col-xs-4 col-md-4 col-sm-4">
+                  <a href="#"><img src="images/1.jpg" alt="">8</a></div>
+                </div>
+          </div>
+          <a class="left carousel-control" href="#myCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
+          <a class="right carousel-control" href="#myCarousel" data-slide="next"><i  class="glyphicon glyphicon-chevron-right"></i></a>
+        </div>
     </div>
-
-
-
+  <form class="" action="index.html" method="post">
+    </div>
+  </form>
     <label for=""></label>
     <input type="text" name="" value="">
     <label for=""></label>
