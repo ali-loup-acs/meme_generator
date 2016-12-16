3896fc811ba5405db75f68812826a2d2b853d3a5
paulinefrqt <pauline.f@codeur.online>
2016-12-16 10:50:36 +0100

    ajout css
    lien css dans header et modifs css

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
   <head>
     <title>memeGenerator</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <meta charset="utf-8">
